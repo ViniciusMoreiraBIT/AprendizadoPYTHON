9b492fcd4b297b619859d2084bc55630fb94209e
Vinicius Moreira Dos Santos <viniciusmoreira.system@gmail.com>
2026-01-19 20:53:13 -0300

    Pausa solução problema 1

--- PYTHON/Mundo 2/Video 49 (condições aninhadas)/#12 036 (Aprovando Emprestimos).py
+++ PYTHON/Mundo 2/Video 49 (condições aninhadas)/#12 036 (Aprovando Emprestimos).py
@@ -1,4 +1,16 @@
 '''Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
-O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagarr.
+O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 Calcule o valor da prestação mensal, sabendo que ela não pode exeder 30% do salário
 ou então o empréstimo será negado'''
+valor=float(input('Valor do imóvel: R$ '))
+salario=float(input('Salário do comprador: R$ '))
+ano=int(input('Pagar por quantos anos: '))
+valor_prestacao= valor / ano
+valor1= salario * ( 30 / 100)
+print(valor_prestacao)
+print (valor1)
+if valor_prestacao > salario * ( 30 / 100):
+    print('Empréstimo negado')
+elif valor_prestacao
+
+
