7fb463a2470f7d82fbe8af6b61fe637e1c81f659
Vinicius Moreira Dos Santos <viniciusmoreira.system@gmail.com>
2026-01-24 18:46:23 -0300

    F

--- PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 048 (Soma ímpares múltiplos de três).py
+++ PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 048 (Soma ímpares múltiplos de três).py
@@ -2,3 +2,7 @@
 entre todos os números ímpares que
 são múltiplos de três e
 que se encontram no intervalo de 1 até 500.'''
+
+for i in range(0, 500 + 1, 3):
+    if i % 2 == 1:
+         print(i)
--- PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 050 (Soma dos Pares).py
+++ PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 050 (Soma dos Pares).py
@@ -2,3 +2,13 @@
 números inteiros e mostre a soma apenas
 daqueles que forem pares. Se o valor
 digitado for impar, desconsidere-o.'''
+soma = 0
+for i in range(1,6+1):
+    num = int(input('Número: '))
+    if num % 2 == 0:
+        soma = num + soma
+        print(soma)
+    else:
+        soma2 = num + soma
+print(f'Pares: {soma}')
+print(f'Impares: {soma2}')
