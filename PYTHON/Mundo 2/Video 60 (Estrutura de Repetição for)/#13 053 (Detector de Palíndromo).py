b36739aee0cf3370d5eeb435643106e3eae0764a
Vinicius Moreira Dos Santos <viniciusmoreira.system@gmail.com>
2026-01-27 21:22:47 -0300

    idade

--- PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 053 (Detector de Palíndromo).py
+++ PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 053 (Detector de Palíndromo).py
@@ -5,3 +5,14 @@ EX: Apos a sopa
 A sacada da casa
 A torre da derrota
 O lobo Ama bolo'''
+
+p = input('Frase: ')
+#fraseS= p.lower().replace(' ','')[::-1]
+#= p.lower().replace(' ','')
+if p.lower().replace(' ','')[::-1] == p.lower().replace(' ',''):
+    print('É um Palindromo!')
+    print(p)
+    print('x')
+    print(p[::-1])
+else:
+    print('Negativo')
--- PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 054 (Grupo da Maioridade).py
+++ PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 054 (Grupo da Maioridade).py
@@ -2,3 +2,24 @@
 nascimento de sete pessoas. No final,
 mostre quantas pessoas ainda não atingiram a maioridade
 e quantas já são maiores'''
+p = []
+idadee = []
+for i in range(1, 7):
+    ano = int(input('Ano de Nascimento: '))
+    p.append(ano)
+
+for i in range(0,6):
+    idade = 2026 - p[i]
+    if idade < 18:
+        print(f'{idade} Anos, ainda não atingiu a maioridade!')
+        idadee.append(idade)
+    else:
+        print(f'{idade} Anos, Maior de idade!')
+        idadee.append(idade)
+
+print(idadee)
+
+
+
+
+
