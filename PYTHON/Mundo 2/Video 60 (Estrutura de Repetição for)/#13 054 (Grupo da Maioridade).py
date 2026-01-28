839f62c23542e34bea86685c9b5941e5d9426ce7
Vinicius Moreira Dos Santos <viniciusmoreira.system@gmail.com>
2026-01-28 19:59:46 -0300

    resolvido

--- PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 054 (Grupo da Maioridade).py
+++ PYTHON/Mundo 2/Video 60 (Estrutura de Repetição for)/#13 054 (Grupo da Maioridade).py
@@ -7,17 +7,31 @@ idadee = []
 for i in range(1, 7):
     ano = int(input('Ano de Nascimento: '))
     p.append(ano)
-
+ma = 0
+me= 0
 for i in range(0,6):
     idade = 2026 - p[i]
     if idade < 18:
         print(f'{idade} Anos, ainda não atingiu a maioridade!')
         idadee.append(idade)
+        me += 1
     else:
         print(f'{idade} Anos, Maior de idade!')
         idadee.append(idade)
+        ma += 1
 
-print(idadee)
+print(f'''
+{ma} pessoas de Maior.
+{me} pessoas de Menor.''')
+
+
+for i in range(0 , 7):
+    if idadee[i] >= 18:
+        print(f'De maior:+ {idadee[i]}')
+    else:
+        print(f'De menor:- {idadee[i]}')
+
+
 
 
 
