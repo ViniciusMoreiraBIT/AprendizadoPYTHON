9e50a5f0334b205c372de1ecc49952793646d1cf
Vinicius Moreira Dos Santos <viniciusmoreira.system@gmail.com>
2026-03-03 20:31:33 -0300

    Progressao aritmetica

--- PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 061 (Progreção Aritmética v2.0).py
+++ PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 061 (Progreção Aritmética v2.0).py
@@ -2,3 +2,10 @@
 termo e a razão de uma PA, mostrando 10
 primeiros termos da progressão usando a estrutura while'''
 
+termo = int(input('Primeiro termo: '))
+PA = int(input('PA: '))
+loop = 1
+while loop <= 10:
+    razao = termo + (loop -1) * PA
+    loop += 1
+    print(f'{loop - 1} termo (a{loop - 1}) = {razao}')
