4512dde30b0ef70a8ff7d98e6df4e39a936eae3e
Vinicius Moreira Dos Santos <viniciusmoreira.system@gmail.com>
2026-03-05 12:07:41 -0300

    resolvendo desafio 065

--- PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 065 (Maior e Menor valores).py
+++ PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 065 (Maior e Menor valores).py
@@ -3,23 +3,39 @@ No final da execução, mostre a média entre todos os valores
 e qual foi o maior e o menor valores lidos. O progama deve
 perguntar ao usúario se ele quer ou não continuar a digitar valores.'''
 
-lista = []
+numbers = []
 print('Digite vários valores, no final vamos mostrar a média desses valores e o Maior! 999 para parar.')
 loop = 0
 soma = 0
 while True:
-    n = int(input(f'{loop + 1}º Valor: '))
+    n = input(f'{loop + 1}º Valor: ')
+    if not n.isdigit():
+        print('Apenas números!')
+        continue
+    else:
+        n = int(n)
+
     if n != 999:
         soma += n
         loop += 1
-        lista.append(n)
+        numbers.append(n)
     if n == 999:
         media = soma / loop
-        print(f'A Média foi: {media:.2f}')
-        res = input(f'O maior número digitado foi: {max(lista)}\nO menor número digitado foi {min(lista)}\nDeseja adicionar mais valores? (S/N)')
-        if res.upper() == 'S':
-            continue
-        elif res.upper() == 'N':
+        print(f'\nA Média foi: {media:.2f}')
+        print(f'O maior número digitado foi: {max(numbers)}\nO menor número digitado foi: {min(numbers)}\n\nDeseja adicionar mais valores? (S/N)')
+        while True:
+            res = input('').upper()
+            if res not in ('S','N'):
+                print('Apenas S/N')
+                continue
+            elif res == 'S' or res == 'N':
+                if res == 'S':
+                    break
+                elif res == 'N':
+                    break
+        if res == 'N':
             break
+
+
     else:
         continue
