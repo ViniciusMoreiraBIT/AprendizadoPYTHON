7289c451b14e14ec82b239fff15a37ea9df3df89
Vinicius Moreira Dos Santos <viniciusmoreira.system@gmail.com>
2026-03-05 22:19:19 -0300

    resoluções dos exercicios 57,58 e 59

--- PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 057 (Validação de Dados).py
+++ PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 057 (Validação de Dados).py
@@ -2,16 +2,26 @@
 mas só aceite os valores M ou F. Caso esteja errado,
 peça a digitação novamente até ter um valor correto.'''
 
-print('Responda M para masculino e F para feminino.')
+#minha resolução ↓
+'''print('Responda M para masculino e F para feminino.')
 while True:
-    sexo = input('Qual seu sexo[M/F]? ').upper()
+    sexo = input('Qual seu sexo[M/F]? ').upper().strip()
     if sexo != 'M' and sexo != 'F':
         print('Valor invalido.')
         continue
     elif sexo == 'M':
-        print('Você pe HOMEM')
+        print('Você é HOMEM')
         break
     else:
         print('Você é MULHER')
-        break
+        break'''
 
+res = str(input('Informe seu sexo (M/F): ')).strip().upper()[0] #[0] pega apenas a primeira letra
+while res not in 'MF':
+    res = str(input('Informe apenas M ou F: ')).upper().strip()[0]
+if res == 'M':
+    res ='Masculino'
+else:
+    res ='Feminino'
+
+print(f'Sexo registrado com Sucesso: {res}')
--- PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 059 (Criando um Menu de Opções).py
+++ PYTHON/Mundo 2/Video 72 (Estrutura de repetição while)/#14 059 (Criando um Menu de Opções).py
@@ -8,17 +8,17 @@ mostre um menu na tela:
 
 Seu programa deverá realizar a operação solicitada em cada caso'''
 
-n1 = int(input('Primeiro número: '))
+#Minha resolução ↓
+'''n1 = int(input('Primeiro número: '))
 n2 = int(input('Segundo número: '))
 while True:
     print('=-'* 10,'\n',' '*6,'MENU\n'+'=-'*10)
-    print('1 SOMA\n2 MULTIPLICAR\n3 MAIOR VALOR\n4 NOVOS NÚMEROS\n5 SAIR DO PROGRAMA')
+    print('1 SOMAR\n2 MULTIPLICAR\n3 MAIOR VALOR\n4 NOVOS NÚMEROS\n5 SAIR DO PROGRAMA')
     res = int(input('Qual deseja? '))
     if res == 4:
         n1 = int(input('\nPrimeiro número: '))
         n2 = int(input('Segundo número: '))
         print('Valores alterados.\n')
-        continue
     elif res == 1:
         print(f'\nA Soma de {n1} + {n2} é {n1 + n2}\n')
     elif res == 2:
@@ -27,3 +27,39 @@ while True:
         print(f'O maior valor ente {n1} e {n2} é {max(n1,n2)}')
     elif res == 5:
         break
+    else:
+        print('\nOpção invalida..\n')'''
+from time import sleep
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+opção = 0
+while opção != 5:
+    print('=-'* 10,'\n',' '*6, 'MENU\n'+'=-'*10)
+    print('1 SOMAR\n2 MULTIPLICAR\n3 MAIOR VALOR\n4 NOVOS NÚMEROS\n5 SAIR DO PROGRAMA')
+    opção = int(input('Qual deseja ? → '))
+    if opção == 1:
+        soma = n1 + n2
+        for i in [n1,'+',n2,'=',soma]:
+            print(i,'',end='')
+            sleep(0.5)
+        print('')
+    elif opção == 2:
+        mult = n1 * n2
+        print(f'{n1} x {n2} = {mult}')
+        sleep(1)
+    elif opção == 3:
+        if n1 > n2:
+            print(f'O maior é {n1}')
+            sleep(1)
+        else:
+            print(f'O maior é {n2}')
+            sleep(1)
+    elif opção == 4:
+        n1 = int(input('Primeiro número: '))
+        n2 = int(input('Segundo número: '))
+        sleep(1)
+        print('Valores atualizados.')
+for i in range(0,3):
+    print('.',end='')
+    sleep(0.4)
+print('\nFim do programa!')
